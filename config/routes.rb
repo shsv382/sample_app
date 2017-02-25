0476b4efa895f245d9111514679807e06e08480c
shsv382 <shsv382@gmail.com>
2017-02-25 16:08:24 +0400

    Added auth form with functional

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  resources :users
+
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  root 'static_pages#home'
+  get "/signup" => 'users#new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
