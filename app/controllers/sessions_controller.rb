980f5915c16d3ac9b6f80f8f775f0be62dc0c78f
shsv382 <shsv382@gmail.com>
2017-02-27 10:59:45 +0400

    Sign-in in progress

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		flash[:success] = "Welcome back, #{user.name}!"
+#  		sign_in user
+  		redirect_to user
+  	else
+  		flash.now[:error] = "Введите корректные данные!"
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  end
+end
