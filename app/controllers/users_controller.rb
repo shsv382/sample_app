56815013b75e4bb70c521b8d11ad1f1d0b34ef0c
shsv382 <shsv382@gmail.com>
2017-03-04 19:00:04 +0400

    Added all-users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
 
-    before_action :signed_in_user, only: [:edit, :update]
+    before_action :signed_in_user, only: [:index, :edit, :update]
     before_action :correct_user,   only: [:edit, :update]
 
   	def index
+      @users = User.all
   	end 
 
   	def show
@@ -49,7 +50,10 @@ class UsersController < ApplicationController
   	end
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
